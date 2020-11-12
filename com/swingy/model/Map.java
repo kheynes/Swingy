2987eaa66d6b1ccacff33fa766c99f873ea81946
kheynes <heyneskeziah@gmail.com>
2020-11-12 22:27:40 +0200

    randomly generates villains

--- com/swingy/model/Map.java
+++ com/swingy/model/Map.java
@@ -1,34 +1,68 @@
 package com.swingy.model;
 
 import com.swingy.model.characters.*;
+import java.util.ArrayList;
+import java.util.Random;
 
 public class Map {
     static public int size;
     static public int[][] map;
+    static SuperVillain villain;
 
     public Map(SuperChampion champion) {
         size = (champion.level - 1) * 5 + 10 - (champion.level % 2);
         map = new int[size][size];
         champion.x = (size / 2) + 1;
         champion.y = (size / 2) + 1;
+        villainPosition(champion);
     }
 
-    public villainPosition() {
-        private int villainCount = size / 3;
+    public void villainPosition(SuperChampion champion) {
+        Random rand = new Random();
+        ArrayList<SuperVillain> villains = new ArrayList<SuperVillain>();
+        int villainCount = 3*size;
+        System.out.println(villainCount);
         int counter = 0;
+        int typeCount = 0;
+        int x;
+        int y;
+        int randomGen = size*size;
         while(counter < villainCount) {
-            
+            x = rand.nextInt(randomGen);
+            y = rand.nextInt(randomGen);
+            while (x == champion.x && y == champion.y) {
+                x = rand.nextInt(size);
+                y = rand.nextInt(size);
+            }
+            for (SuperVillain vil : villains) {
+                if (x == vil.x && y == vil.y) {
+                    x = rand.nextInt(randomGen);
+                    y = rand.nextInt(randomGen);
+                }
+            }
+            if ((typeCount < villainCount/3)) {
+                villain = new Villain_Slime();
+                villain.x = x;
+                villain.y = y;
+                villains.add(villain);
+                typeCount++;
+            }else if ((typeCount >= villainCount/3) && (typeCount < (villainCount/3 + villainCount/3))) {
+                villain = new Villain_Wraith();
+                villain.x = x;
+                villain.y = y;
+                villains.add(villain);
+                typeCount++;
+            }else if ((typeCount >= (villainCount/3 + villainCount/3)) && (typeCount < villainCount)) {
+                villain = new Villain_Dragon();
+                villain.x = x;
+                villain.y = y;
+                villains.add(villain);
+                typeCount++;
+            }
+             counter++;
+        }
+        for (SuperVillain vil: villains) {
+            System.out.println(vil.villainName + " " + vil.x + " " + vil.y);
         }
     }
 }
-
-
-
-// int villains = random thing(5)
-// int counter
-// while (counter < villains)
-// -->xy values
-// --> recursive check(xy)
-// --> make SuperVillain
-// --> superVillain.x and superVillain.y = xy
-// --> add to list
--- com/swingy/model/characters/Villain_Bat.java
+++
@@ -1,9 +0,0 @@
-package com.swingy.model.characters;
-
-public class Villain_Bat extends SuperVillain {
-    public Villain_Bat() {
-        villainName = "Bat";
-        hitPoints = 10;
-        attack = 5;
-    }
-}
