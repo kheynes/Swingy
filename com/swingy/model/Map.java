e5be5f14647ae8125a9f4fe75128dc80c1deff0a
kheynes <heyneskeziah@gmail.com>
2020-12-21 21:50:50 +0200

    Just added some colour

--- com/swingy/model/Map.java
+++ com/swingy/model/Map.java
@@ -6,6 +6,9 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Map {
+    public static final String TEXT_RED = "\u001B[31m";
+    public static final String TEXT_GREEN = "\u001B[32m";
+    public static final String TEXT_RESET = "\u001B[0m";
     static public int size;
     static public int[][] map;
     static SuperVillain villain;
@@ -100,7 +103,13 @@ public class Map {
         System.out.println("\n");
         for (int i=0; i < size; i++) {
             for (int j=0; j < size ; j++) {
-                System.out.print(map[i][j] + " ");
+                if (map[i][j] == 1) {
+                    System.out.print(TEXT_RED + map[i][j] + TEXT_RESET + " ");
+                } else if (map[i][j] == 2) {
+                    System.out.print(TEXT_GREEN + map[i][j] + TEXT_RESET + " ");
+                } else {
+                    System.out.print(map[i][j] + " ");
+                }
             }
             System.out.println("\n");
         }
